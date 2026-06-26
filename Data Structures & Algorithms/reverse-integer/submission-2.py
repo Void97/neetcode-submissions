085612c1e70f60930a065788c17060be32a3a601
Vasilii Zaitsev <69293311+Void97@users.noreply.github.com>
2026-06-26 13:32:58 +0800

    Add: reverse-integer - submission-2

---
+++ Data Structures & Algorithms/reverse-integer/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        res = list(str(abs(x)))
+
+        l = 0
+        r = len(res) - 1
+        while l < r:
+            res[l], res[r] = res[r], res[l]
+
+            l += 1
+            r -= 1
+        
+        res = ''.join(res)
+        if int(res) >= -2**31 and int(res) <= 2**31-1 :
+            return int(res) if x > 0 else -int(res)
+        else:
+            return 0
